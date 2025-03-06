bd5e0002777ad5b1bbb5bb7153b50a34ec80106c
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-03-06 18:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let text=document.getElementById("text");
+let number=document.getElementById("delay");
+let btn=document.getElementById("btn");
+
+let promise1=()=>{
+	
+	return new Promise((resolve,reject)=>{
+		let delay = Number(number.value);
+	        if (delay < 0 || number.value === "") {
+            reject("Please enter a valid positive number for the delay.");
+            return;
+        }
+		setTimeout(()=>{
+			resolve(text.value);
+		},delay);
+	});
+}
+async function fetchData()
+{
+	let data = await promise1();
+	let output= document.getElementById("output");
+	output.innerHTML=`<p>${data}</p>
+	<p>${Number(number.value)}</p>`
+}
+btn.addEventListener("click",fetchData);
